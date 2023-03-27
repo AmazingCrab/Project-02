6e8ad1ba0850ce9ee40b8cb48d5fa0b2ff0493f2
Fenush <econtegarcia@gmail.com>
2023-03-27 01:29:13 -0300

    Game Loop f(x) Added

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
 let computerSelection = ""; /*This var store the computer selection*/
 let playerSelection = ""; /*This var store the player selection input*/
 let gameWinner = ""; /*This var display the match result text*/
+let i = 0; /*This variable initialize a counter*/
 /*Variables declaration*/
 
-/*Computer Choice f(x) Start here*/ /*Computer Choice f(x) Start here*/
+/*Computer Choice f(x) & Start here*/ /*Computer Choice f(x) & Start here*/
 function getComputerChoice() {
   let randomNumber = Math.round(
     Math.random() * 10
@@ -16,9 +17,9 @@ function getComputerChoice() {
     computerSelection = "PAPER";
   }
   return computerSelection;
-} /*Computer Choice f(x) Finish here*/ /*Computer Choice f(x) Finish here*/
+} /*Computer Choice f(x) & Finish here*/ /*Computer Choice f(x) & Finish here*/
 
-/*Player Selection f(x) Start here*/ /*Player Selection f(x) Start here*/
+/*Player Selection f(x) & Start here*/ /*Player Selection f(x) & Start here*/
 function playerSelectionCheck() {
   playerSelection = prompt(
     "Choose and start playing the game!",
@@ -37,9 +38,9 @@ function playerSelectionCheck() {
   } else {
     return playerSelection;
   }
-} /*Player Selection f(x) Finish here*/ /*Player Selection f(x) Finish here*/
+} /*Player Selection f(x) & Finish here*/ /*Player Selection f(x) & Finish here*/
 
-/*Tied Game check f(x) Start here*/ /*Tied Game f(x) Start here*/
+/*Tied Game check f(x) & Start here*/ /*Tied Game f(x) & Start here*/
 function gameTied() {
   if (playerSelection === computerSelection) {
     gameWinner = "Tied game, try again!";
@@ -47,9 +48,9 @@ function gameTied() {
     noTiedGame();
   }
   return gameWinner;
-} /*Tied Game check f(x) Finish here*/ /*Tied Game f(x) Finish here*/
+} /*Tied Game check f(x) & Finish here*/ /*Tied Game f(x) & Finish here*/
 
-/*No tied game f(x) Start here*/ /*No tied game f(x) Start here*/
+/*No tied game f(x) & Start here*/ /*No tied game f(x) & Start here*/
 function noTiedGame() {
   gameWinner = "You win the match! You are amazing!";
   switch (playerSelection) {
@@ -73,15 +74,24 @@ function noTiedGame() {
     }
   }
   return gameWinner;
-} /*No tied game f(x) Finish here*/ /*No tied game f(x) Finish here*/
+} /*No tied game f(x) & Finish here*/ /*No tied game f(x) & Finish here*/
 
-/*This is the game main F(x) Start here*/ /*This is the game main F(x) Start here*/
+/*This is the game main F(x) & Start here*/ /*This is the game main F(x) & Start here*/
 function game() {
   getComputerChoice();
   playerSelectionCheck();
   gameTied();
   alert(gameWinner);
   gameLoop(); /*We add all functions, and a new last loop f(x)*/
-} /*This is the game main F(x) Finish Here*/ /*This is the game main F(x) Finish here*/
+} /*This is the game main F(x) & Finish Here*/ /*This is the game main F(x) & Finish here*/
 
-game();
+/*This is the game loop f(x) & Start Here*/  /*This is the game loop f(x) & Start Here*/
+function gameLoop() {
+    if (i < 5){
+        i+=1;
+        game();
+    }   
+}
+/*This is the game loop f(x) & Finish Here*/ /*This is the game loop f(x) & Finish Here*/
+
+gameLoop();
