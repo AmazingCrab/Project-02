abc957d87dfcc551227af1eb98ec9b399f9a348c
Fenush <econtegarcia@gmail.com>
2023-03-26 23:08:55 -0300

    Computer Choice f(x) Added

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-/*Variables here*/
+/*Variables declaration*/
 let computerSelection = ""
+let playerSelection = "";
 
-/*Variables here*/
+/*Variables declaration*/
 
-function getComputerChoice();   {   /*Computer Selection f(x) Start here*/
-    let randomNumber = Math.round(Math.random()*10);
+
+/*Computer Choice f(x) Start here*/      /*Computer Choice f(x) Start here*/
+function getComputerChoice();   {   
+    let randomNumber = Math.round(Math.random()*10); /*This make a randomNumber from 1 to 10*/
     computerSelection = "SCISSOR";
     if  (randomNumber <= 3) {
         computerSelection ="ROCK";
@@ -12,5 +15,5 @@ function getComputerChoice();   {   /*Computer Selection f(x) Start here*/
         computerSelection = "PAPER"
     }
     return  computerSelection;
-}                                   /*Computer Selection f(x) Finish here*/
-
+}
+/*Computer Choice f(x) Finish here*/      /*Computer Choice f(x) Finish here*/      
