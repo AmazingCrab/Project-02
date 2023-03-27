d1a7880e19accff05704ab83ea02188a4e683499
Fenush <econtegarcia@gmail.com>
2023-03-27 00:04:57 -0300

    PlayerSelection f(x) Added

--- index.js
+++ index.js
@@ -1,29 +1,43 @@
 /*Variables declaration*/
-let computerSelection = ""
+let computerSelection = "";
 let playerSelection = "";
 
 /*Variables declaration*/
 
-
-/*Computer Choice f(x) Start here*/      /*Computer Choice f(x) Start here*/
-function getComputerChoice();   {   
-    let randomNumber = Math.round(Math.random()*10); /*This make a randomNumber from 1 to 10*/
-    computerSelection = "SCISSOR";
-    if  (randomNumber <= 3) {
-        computerSelection ="ROCK";
-    }   else if (randomNumber <=6)   {
-        computerSelection = "PAPER"
-    }
-    return  computerSelection;
+/*Computer Choice f(x) Start here*/ /*Computer Choice f(x) Start here*/
+function getComputerChoice() {
+  let randomNumber = Math.round(
+    Math.random() * 10
+  ); /*This make a randomNumber from 1 to 10*/
+  computerSelection = "SCISSOR";
+  if (randomNumber <= 3) {
+    computerSelection = "ROCK";
+  } else if (randomNumber <= 6) {
+    computerSelection = "PAPER";
+  }
+  return computerSelection;
 }
-/*Computer Choice f(x) Finish here*/      /*Computer Choice f(x) Finish here*/      
+/*Computer Choice f(x) Finish here*/ /*Computer Choice f(x) Finish here*/
 
-/*Player Selection f(x) Spell Check Start here*/        /*Player Selection f(x) Spell Check Start here*/
-function    playerSelectionCheck()   {
-    playerSelection = prompt("Choose and start playing the game!", "ROCK, PAPER or SCISSOR");
-    playerSelection = playerSelection.toUpperCase();    /*This ignore case sensitive later*/
-    if (!(playerSelection === "ROCK")||(playerSelection === "PAPER")||(playerSelection === "SCISSOR")){
-        playerSelectionCheck();
-    }
+/*Player Selection f(x) Start here*/ /*Player Selection f(x) Start here*/
+function playerSelectionCheck() {
+  playerSelection = prompt(
+    "Choose and start playing the game!",
+    "ROCK, PAPER or SCISSOR"
+  );
+  playerSelection =
+    playerSelection.toUpperCase(); /*This ignore case sensitive later*/
+  if (!(
+    (playerSelection === "ROCK") ||
+    (playerSelection === "PAPER") ||
+    (playerSelection === "SCISSOR")
+  )) {
+    playerSelectionCheck();
+  } else {
+    return playerSelection;
+  }
 }
-/*Player Selection f(x) Spell Check Finish here*/       /*Player Selection f(x) Spell Check Finish here*/
+/*Player Selection f(x) Finish here*/ /*Player Selection f(x) Finish here*/
+
+getComputerChoice();
+playerSelectionCheck();
