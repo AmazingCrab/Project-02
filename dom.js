025f8001d28f3378016c629a605099aac0cdf5ff
Fenush <econtegarcia@gmail.com>
2023-05-02 15:39:05 -0300

    Dom variables initialized

---
+++ dom.js
@@ -0,0 +1,8 @@
+const sectionLanguage = document.querySelector("#sectionLanguage");
+const sectionMain = document.querySelector("#sectionMain");
+const sectionBackImage = document.querySelector("#sectionBackImage");
+
+const spanish = document.querySelector("#flagSpanish");
+const english = document.querySelector("#flagEnglish");
+const h3 = document.querySelector("h3");
+const h2 = document.querySelector("h2");
