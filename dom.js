adf22aa8e3477d9f899bfa41c478898b6687a548
Fenush <econtegarcia@gmail.com>
2023-05-02 15:41:01 -0300

    Initializated ictionaries

--- dom.js
+++ dom.js
@@ -6,3 +6,13 @@ const spanish = document.querySelector("#flagSpanish");
 const english = document.querySelector("#flagEnglish");
 const h3 = document.querySelector("h3");
 const h2 = document.querySelector("h2");
+
+const dictionarySpanish = [
+    "Elije tu idioma",    //h3 
+    "Vamos a jugar Piedra Papel o Tijera!",   // h2
+  ]; // Spa. dictionary words
+  
+  const dictionaryEnglish = [
+    "Choose your language",   //h3 
+    "Lets Play Rock, Paper,Scissor!", // h2
+  ]; // Eng. dictionary words
