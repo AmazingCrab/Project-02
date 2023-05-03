57b7e7d52105a70f723c7f39d3d71dd9b176dd95
Fenush <econtegarcia@gmail.com>
2023-05-02 22:33:54 -0300

    index.js enable

--- dom.js
+++ dom.js
@@ -12,82 +12,95 @@ const paper = document.querySelector("#paper");
 const scissor = document.querySelector("#scissor");
 
 const dictionarySpanish = [
-    "Elije tu idioma",    //h3SelectLenguage 
-    "Vamos a jugar Piedra Papel o Tijera!",   //h2Title
-    "Selecciona PIDRA, PAPEL ó TIJERA!",  //h3SubTitle
-    "Piedra", // button 1
-    "Papel",  // button 2
-    "Tijera"  // button 3
-  ]; // Spa. dictionary words
-  
+  "Elije tu idioma", //h3SelectLenguage
+  "Vamos a jugar Piedra Papel o Tijera!", //h2Title
+  "Selecciona PIDRA, PAPEL ó TIJERA!", //h3SubTitle
+  "Piedra", // button 1
+  "Papel", // button 2
+  "Tijera", // button 3
+]; // Spa. dictionary words
+
 const dictionaryEnglish = [
-    "Choose your language",   //h3SelectLenguage 
-    "Lets Play Rock, Paper,Scissor!", //h2Title
-    "Select Between ROCK PAPER SCISSORS!", //h3SubTitle
-    "Rock",   // button 1
-    "Paper",  // button 2
-    "Scissor" // button 3
-  ]; // Eng. dictionary words
+  "Choose your language", //h3SelectLenguage
+  "Lets Play Rock, Paper,Scissor!", //h2Title
+  "Select Between ROCK PAPER SCISSORS!", //h3SubTitle
+  "Rock", // button 1
+  "Paper", // button 2
+  "Scissor", // button 3
+]; // Eng. dictionary words
 
 let dictionary = dictionaryEnglish; // Selected default dictionary
 
-// Vars. initialized 
+// Vars. initialized
 h3SelectLenguage.textContent = dictionary[0];
 h2Title.textContent = dictionary[1];
 h3SubTitle.textContent = dictionary[2];
 rock.textContent = dictionary[3];
 paper.textContent = dictionary[4];
 scissor.textContent = dictionary[5];
-sectionLanguage.classList.add('clicked');
+sectionLanguage.classList.add("clicked");
 
 function translateSpanish(e) {
-    sectionLanguage.classList.add('clicked');
-    dictionary = dictionarySpanish;
-    english.classList.remove("clicked", "click"); // remove if present
-    h2Title.classList.remove('select');  // remove if present
-    sectionMain.classList.remove('clicked'); // remove if present
-    spanish.classList.add("click");
-    h3SelectLenguage.classList.add("select");
-    h3SelectLenguage.textContent = dictionary[0]; // later we make this a f() with all the words
-    h2Title.textContent = dictionary[1];
-    h3SubTitle.textContent = dictionary [2];
-    rock.textContent = dictionary [3];
-    paper.textContent = dictionary [4];
-    scissor.textContent = dictionary [5];
-    setTimeout(function () {
-      spanish.classList.remove("click");
-      h3SelectLenguage.classList.remove("select");
-      sectionLanguage.classList.remove('clicked');
-      sectionMain.classList.add('clicked');   // next step
-      h2Title.classList.add('select'); // next step
-    }, 2000);
-    spanish.classList.add("clicked");
-  }
-  
-  function translateEnglish(e) {
-    sectionLanguage.classList.add('clicked');
-    dictionary = dictionaryEnglish;
-    spanish.classList.remove("clicked", "click"); // remove if present
-    h2Title.classList.remove('select');  // remove if present
-    sectionMain.classList.remove('clicked'); // remove if present
-    english.classList.add("click");
-    h3SelectLenguage.classList.add("select");
-    h3SelectLenguage.textContent = dictionary[0];
-    h2Title.textContent = dictionary[1];
-    h3SubTitle.textContent = dictionary[2]
-    rock.textContent = dictionary [3];
-    paper.textContent = dictionary [4];
-    scissor.textContent = dictionary [5];
-    setTimeout(function () {
-      english.classList.remove("click");
-      h3SelectLenguage.classList.remove("select");
-      sectionLanguage.classList.remove('clicked');
-      sectionMain.classList.add('clicked'); // next step
-      h2Title.classList.add('select'); // next step
-    }, 2000);
-    english.classList.add("clicked");
-  }
-  
-  spanish.addEventListener("click", translateSpanish, { capture: false });
-  english.addEventListener("click", translateEnglish, { capture: false });
+  sectionLanguage.classList.add("clicked");
+  dictionary = dictionarySpanish;
+  english.classList.remove("clicked", "click"); // remove if present
+  h2Title.classList.remove("select"); // remove if present
+  sectionMain.classList.remove("clicked"); // remove if present
+  spanish.classList.add("click");
+  h3SelectLenguage.classList.add("select");
+  h3SelectLenguage.textContent = dictionary[0]; // later we make this a f() with all the words
+  h2Title.textContent = dictionary[1];
+  h3SubTitle.textContent = dictionary[2];
+  rock.textContent = dictionary[3];
+  paper.textContent = dictionary[4];
+  scissor.textContent = dictionary[5];
+  setTimeout(function () {
+    spanish.classList.remove("click");
+    h3SelectLenguage.classList.remove("select");
+    sectionLanguage.classList.remove("clicked");
+    sectionMain.classList.add("clicked"); // next step
+    h2Title.classList.add("select"); // next step
+  }, 2000);
+  spanish.classList.add("clicked");
+}
 
+function translateEnglish(e) {
+  sectionLanguage.classList.add("clicked");
+  dictionary = dictionaryEnglish;
+  spanish.classList.remove("clicked", "click"); // remove if present
+  h2Title.classList.remove("select"); // remove if present
+  sectionMain.classList.remove("clicked"); // remove if present
+  english.classList.add("click");
+  h3SelectLenguage.classList.add("select");
+  h3SelectLenguage.textContent = dictionary[0];
+  h2Title.textContent = dictionary[1];
+  h3SubTitle.textContent = dictionary[2];
+  rock.textContent = dictionary[3];
+  paper.textContent = dictionary[4];
+  scissor.textContent = dictionary[5];
+  setTimeout(function () {
+    english.classList.remove("click");
+    h3SelectLenguage.classList.remove("select");
+    sectionLanguage.classList.remove("clicked");
+    sectionMain.classList.add("clicked"); // next step
+    h2Title.classList.add("select"); // next step
+  }, 2000);
+  english.classList.add("clicked");
+}
+
+spanish.addEventListener("click", translateSpanish, { capture: false }); // choose lenguage spanish f()
+english.addEventListener("click", translateEnglish, { capture: false }); // // choose lenguage english f()
+
+function rockSelection (e){
+  console.log(e);
+}
+
+function paperSelection (e){
+}
+
+function scissorSelection (e){
+}
+
+rock.addEventListener("click", rockSelection, {capture: false });
+paper.addEventListener("click", paper,{capture: false });
+scissor.addEventListener("click", scissor,{capture: false });
